dcccec01c12f26274c004467dd249208d332eb5b
bartektomczyk <bartlomiej.tomczyk@callstack.com>
2022-11-27 11:55:47 +0100

    added state-based navgation and add first animation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,9 +8,11 @@
  * @format
  */
 
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {
+  Pressable,
   SafeAreaView,
+  ScrollView,
   StatusBar,
   Text,
   useColorScheme,
@@ -18,23 +20,57 @@ import {
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {BlinkingAnimated} from './Blinking/BlinkingAnimated';
+
+const animations = [
+  {label: 'Blinking', component: <BlinkingAnimated />},
+  {label: 'Another 1', component: null},
+  {label: 'Another 1', component: null},
+  {label: 'Another 1', component: null},
+  {label: 'Another 1', component: null},
+  {label: 'Another 1', component: null},
+  {label: 'Another 1', component: null},
+  {label: 'Another 1', component: null},
+  {label: 'Another 1', component: null},
+];
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
-
+  const [currentAnimation, setCurrentAnimation] = useState(0);
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+  const handleChangeAnimation = useCallback((index: number) => {
+    setCurrentAnimation(index);
+  }, []);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={{...backgroundStyle, flex: 1}}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <View>
-        <Text>Content</Text>
-      </View>
+      <ScrollView horizontal style={{backgroundColor: '#ddd', flexGrow: 0}}>
+        {animations.map(({label}, index) => (
+          <Pressable
+            key={index}
+            style={{
+              paddingVertical: 5,
+              paddingHorizontal: 10,
+              margin: 5,
+            }}
+            accessibilityRole="button"
+            onPress={() => handleChangeAnimation(index)}>
+            <Text
+              style={{
+                color: currentAnimation === index ? '#1ce1ce' : '#333',
+              }}>
+              {label}
+            </Text>
+          </Pressable>
+        ))}
+      </ScrollView>
+      <View style={{flex: 1}}>{animations[currentAnimation].component}</View>
     </SafeAreaView>
   );
 };
